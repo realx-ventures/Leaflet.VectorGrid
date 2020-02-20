9e6df7fdd7c32f41b12149d3ed942164804ac3a2
Dan Korhnak <dankorhnak@gmail.com>
2020-02-20 12:38:20 -0500

    replace fetch.fetch with fetch

--- dist/Leaflet.VectorGrid.bundled.js
+++ dist/Leaflet.VectorGrid.bundled.js
@@ -2807,7 +2807,7 @@ L.VectorGrid.Protobuf = L.VectorGrid.extend({
 		var tileUrl = L.Util.template(this._url, L.extend(data, this.options));
 
 		var controller = this._abortController;
-		return fetch$1.fetch(tileUrl, Object.assign({signal: controller.signal}, this.options.fetchOptions)).then(function(response){
+		return fetch$1(tileUrl, Object.assign({signal: controller.signal}, this.options.fetchOptions)).then(function(response){
 
 			if (!response.ok || !this._isCurrentTile(coords)) {
 				return {layers:[]};
